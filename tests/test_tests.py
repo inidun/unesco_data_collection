32530f1708750a897338f7045b41b75c3afc1ee0
Andreas Marklund <ansmad00@ad.umu.se>
2020-05-13 17:08:53 +0200

    added ConventionIndexParser

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -2,27 +2,21 @@ import pytest
 import requests
 
 import sys
-sys.path.append(".")
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 
-from src.conventions.convention_parser import ConventionParser
+
+from conventions.convention_parser import ConventionParser
+from conventions.convention_parser import ConventionIndexParser
 
 def test_if_test_working():
     assert True
 
 
-# url = "http://portal.unesco.org/en/ev.php-URL_ID=13517&URL_DO=DO_TOPIC&URL_SECTION=201.html#ENTRY"
-# response = requests.get(url)
-
-# assert response is not None
-# assert response.status_code == 200
-
-# page = response.text
-
-
 def test_extract__given_a_valid_html__convention_text_retrieved_as_expected():
 
     # Arrange
-    with open("./tests/fixtures/sample_response_page.html", "r") as fp:
+    with open("tests/fixtures/sample_response_page.html", "r") as fp:
         page = fp.read()
 
     # Act
@@ -30,12 +24,13 @@ def test_extract__given_a_valid_html__convention_text_retrieved_as_expected():
     result = sut.extract(page)
 
     # Assert
-
     assert result is not None
     assert len(result) > 0
     assert "Article" in result
 
-def test_extract_text_lincs__given_a_valid_index_html__returns_list_of_convention_page_urls():
+def test_extract_text_links__given_a_valid_index_html__returns_list_of_convention_page_urls():
+
+    # http://portal.unesco.org/en/ev.php-URL_ID=12025&URL_DO=DO_TOPIC&URL_SECTION=-471.html
 
     # Arrange
     with open("./tests/fixtures/sample_response_index_page.html", "r") as fp:
@@ -46,8 +41,7 @@ def test_extract_text_lincs__given_a_valid_index_html__returns_list_of_conventio
     result = sut.extract(page)
 
     # Assert
-
     assert result is not None
-    assert len(result) == 26
+    assert len(result) == 32
 
 
