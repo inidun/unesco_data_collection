58088da3a87f4e02e450a8f31a4d0eb1d899cdbb
aibakeneko <aibakeneko@gmail.com>
2021-03-31 17:36:12 +0200

    Refactor: extracted method

--- courier/overlap_check.py
+++ courier/overlap_check.py
@@ -1,29 +1,35 @@
 import pandas as pd
-import ast
 
-from courier.courier_metadata import create_article_index
+# import ast
 
-df = create_article_index("../data/courier/UNESCO_Courier_metadata.csv")
-df = df[["courier_id", "record_number", "pages"]]
+# from courier.courier_metadata import create_article_index
+# df = create_article_index("../data/courier/UNESCO_Courier_metadata.csv")
+# df["pages"] = df["pages"].apply(lambda x: ast.literal_eval(str(x)))
 
-df["pages"] = df["pages"].apply(lambda x: ast.literal_eval(str(x)))
-df_pages = df.explode("pages")
 
-page_count = df_pages.groupby(["courier_id", "pages"]).size()
-page_count = page_count.reset_index()
-page_count.columns = ["courier_id", "pages", "count"]
+def get_overlapping_pages(article_index: pd.DataFrame):
+    df_pages = article_index[["courier_id", "record_number", "pages"]].explode("pages")
+    page_count = df_pages.groupby(["courier_id", "pages"]).size()
+    page_count = page_count.reset_index()
+    page_count.columns = ["courier_id", "pages", "count"]
+    overlapping_pages = page_count[page_count["count"] > 1]
+    overlapping_pages = overlapping_pages.rename(columns={"pages": "page"})
+    return overlapping_pages
 
-overlapping_pages = page_count[page_count["count"] > 1]
-overlapping_pages = overlapping_pages.rename(columns={"pages": "page"})
 
 def save_overlapping_pages(overlap_df: pd.DataFrame) -> None:
     overlap_df.to_csv("../data/courier/overlapping_pages.csv", sep="\t", index=False)
 
+
 def create_copy_script(overlap_df: pd.DataFrame) -> None:
-    overlap_df["copy"] = overlap_df.apply(lambda x: f'cp {int(x["courier_id"]):06}eng?_{x["page"]:04}.txt ./tmp', axis=1)
+    overlap_df["copy"] = overlap_df.apply(
+        lambda x: f'cp {int(x["courier_id"]):06}eng*_{x["page"]:04}.txt ./tmp', axis=1
+    )
     d = overlap_df["copy"]
     d.to_csv("copy.sh", index=False)
 
+
+# get_overlapping_pages(df)
 # save_overlapping_pages(overlapping_pages)
 # create_copy_script(overlapping_pages)
 
