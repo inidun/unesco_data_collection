f2daf4f40c185bfe82a3ae25f725d921931a51a9
aibakeneko <aibakeneko@gmail.com>
2021-04-03 19:49:43 +0200

    Fixed lambda expression, added config

--- courier/split_article_pages.py
+++ courier/split_article_pages.py
@@ -7,18 +7,17 @@ import pandas as pd
 
 from courier.courier_metadata import create_article_index
 from courier.overlap_check import get_overlapping_pages
-from courier.extract_articles import read_double_pages_from_file
+from courier.config import CourierConfig
 
-DOUBLE_PAGES_FILE = "../data/courier/double_pages/double_pages.txt"
-EXCLUSIONS_FILE = "../data/courier/double_pages/exclude.txt"
+CONFIG = CourierConfig()
 
 def create_regexp(title_string: str) -> str:
     tokens = re.findall("[a-zåäö]+", title_string.lower())
     expression = "[^a-zåäö]+".join(tokens)
     return expression[1:]
 
-article_index = create_article_index("../data/courier/UNESCO_Courier_metadata.csv")
-double_pages = read_double_pages_from_file(DOUBLE_PAGES_FILE, EXCLUSIONS_FILE)
+article_index = create_article_index(CONFIG.courier_metadata)
+double_pages = CONFIG.double_pages
 
 df_overlap = get_overlapping_pages(article_index)
 
@@ -26,7 +25,7 @@ found_stats = []
 
 for row in df_overlap.to_dict(orient="records"):
 
-    articles_on_page = article_index[(article_index.courier_id == row["courier_id"]) & article_index.pages.apply(lambda x: row["page"] in x)]
+    articles_on_page = article_index[(article_index.courier_id == row["courier_id"]) & article_index.pages.apply(lambda x, r=row: r["page"] in x)]
 
     if row["count"] != len(articles_on_page):
         print(f'Page count mismatch: {row["courier_id"]}')
@@ -37,7 +36,7 @@ for row in df_overlap.to_dict(orient="records"):
     row_page = row["page"] - page_delta
 
     page_filename_pattern = f'{int(row["courier_id"]):06}eng*_{row_page:04}.txt'
-    page_filenames = glob.glob(os.path.join("../data/courier/pdfbox_output", page_filename_pattern))
+    page_filenames = glob.glob(os.path.join(CONFIG.pdfbox_txt_dir, page_filename_pattern))
 
     if len(page_filenames) == 0:
         print(f'no match for {row["courier_id"]}')
