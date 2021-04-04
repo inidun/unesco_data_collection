58a472feed1ca4bcef9dfdb01a9d20ad6b6f9747
aibakeneko <aibakeneko@gmail.com>
2021-04-04 11:44:05 +0200

    CourierIssue args, read_xml, FutureWarning

--- courier/elements.py
+++ courier/elements.py
@@ -1,8 +1,23 @@
+import io
+import os
 import re
-from typing import Iterator, List, Tuple
+import warnings
+from typing import Iterator, List, Tuple, Union
 
-import pandas as pd
 import untangle
+
+from courier.config import CourierConfig
+
+CONFIG = CourierConfig()
+
+
+def read_xml(filename: Union[str, bytes, os.PathLike]) -> untangle.Element:
+    with open(filename, "r") as fp:
+        content = fp.read()
+        content = CONFIG.invalid_chars.sub("", content)
+        xml = io.StringIO(content)
+        element = untangle.parse(xml)
+        return element
 
 
 class Page:
@@ -46,16 +61,17 @@ class Article:
 
 
 class CourierIssue:
-    def __init__(self, issue_index: pd.DataFrame, issue: untangle.Element, double_pages: List[int]):
-        self.issue_index = issue_index
-        self.issue = issue
-        # FIXME: get double_pages from config
-        self.double_pages = double_pages
+    def __init__(self, courier_id: str):
+        self.issue_index = CONFIG.article_index.loc[CONFIG.article_index["courier_id"] == courier_id]
+        self.issue = read_xml(list(CONFIG.pdfbox_xml_dir.glob(f'{courier_id}*.xml'))[0])
+        self.double_pages = CONFIG.double_pages.get(courier_id, [])
 
     @property
     def articles(self) -> Iterator[Article]:
-        for record in self.issue_index.to_dict("record"):
-            yield Article(record, self)
+        with warnings.catch_warnings():
+            warnings.simplefilter(action='ignore', category=FutureWarning)
+            for record in self.issue_index.to_dict("record"):
+                yield Article(record, self)
 
     @property
     def num_articles(self) -> int:
