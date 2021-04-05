0ac29712099b07a2217a9e2e45ad6467d982051e
aibakeneko <aibakeneko@gmail.com>
2021-04-05 19:38:35 +0200

    Added get_stats

--- courier/utils.py
+++ courier/utils.py
@@ -1,6 +1,11 @@
 import os
 from pathlib import Path
-from typing import List, Union
+from statistics import median
+from typing import Dict, List, Union
+
+import pdf2image
+
+from courier.config import CourierConfig
 
 
 def get_filenames(files: Union[str, os.PathLike]) -> List[Path]:
@@ -11,3 +16,15 @@ def get_filenames(files: Union[str, os.PathLike]) -> List[Path]:
     elif path.is_file() and path.suffix == ".pdf":
         items.append(path)
     return items
+
+
+def get_stats() -> Dict[str, int]:
+    tot_pages = []
+    for file in Path(CourierConfig.pdf_dir).glob('*.pdf'):
+        tot_pages.append(pdf2image.pdfinfo_from_path(file)["Pages"])
+    return {
+        "files": len(tot_pages),
+        "pages": sum(tot_pages),
+        "mean": round(sum(tot_pages) / len(tot_pages)),
+        "median": round(median(tot_pages)),
+    }
