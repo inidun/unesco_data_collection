7fafa7997429774c4151b49b9aa7f93c128d8b60
aibakeneko <aibakeneko@gmail.com>
2021-04-05 19:39:12 +0200

    fixed tests

--- tests/courier_extract_text_test.py
+++ tests/courier_extract_text_test.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 import pdfbox
-import pytest
 
 from courier.config import CourierConfig
 from courier.extract_text_pdfbox import extract_text as extract_text_pdfbox
@@ -15,8 +14,8 @@ CONFIG = CourierConfig()
 # Test if python-pdfbox works
 def test_python_pdfbox_extract_text_generates_correct_output():
 
-    file = CONFIG.test_files_dir / "112488eng.pdf"
-    expected_output = CONFIG.test_files_dir / "112488eng.txt"
+    file = CONFIG.test_files_dir / "test.pdf"
+    expected_output = CONFIG.test_files_dir / "test.txt"
     p = pdfbox.PDFBox()
 
     with TemporaryDirectory() as output_dir:
@@ -28,17 +27,17 @@ def test_python_pdfbox_extract_text_generates_correct_output():
         assert filecmp.cmp(output_path, expected_output) is True
 
 
-@pytest.mark.skip(reason="Too slow, test with fewer files")
 def test_extract_text_pdfbox_generates_expected_output():
     with TemporaryDirectory() as output_dir:
-        extract_text_pdfbox(CONFIG.test_files_dir, output_dir)
+        extract_text_pdfbox(CONFIG.test_files_dir / "test.pdf", output_dir)
 
-        assert len(list(Path(output_dir).iterdir())) == 212
-        assert filecmp.dircmp(output_dir, CONFIG.test_files_dir / "pdfbox_out").diff_files == []
+        assert len(list(Path(output_dir).iterdir())) == 8
+        assert filecmp.dircmp(output_dir, CONFIG.test_files_dir / "expected").diff_files == []
+        assert len(filecmp.dircmp(output_dir, CONFIG.test_files_dir / "not_expected").diff_files) == 1
 
 
 def test_extract_text_tesseract_extracts_right_amount_of_files():
     with TemporaryDirectory() as output_dir:
         extract_text_tesseract(CONFIG.test_files_dir, output_folder=output_dir, dpi=1, fmt="png")
 
-        assert len(list(Path(output_dir).iterdir())) == 212
+        assert len(list(Path(output_dir).iterdir())) == 8
