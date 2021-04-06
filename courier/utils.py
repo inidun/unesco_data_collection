dcb699b12839ef77a9502928236435bc0980980c
aibakeneko <aibakeneko@gmail.com>
2021-04-06 11:33:24 +0200

    added get_double_pages

--- courier/utils.py
+++ courier/utils.py
@@ -1,11 +1,18 @@
+import csv
+import functools
+import operator
 import os
 from pathlib import Path
 from statistics import median
-from typing import Dict, List, Union
+from typing import Any, Dict, Iterable, List, Union
 
 import pdf2image
 
 from courier.config import CourierConfig
+
+
+def flatten(list_of_list: Iterable[Iterable[Any]]) -> List[Any]:
+    return functools.reduce(operator.iconcat, list_of_list, [])
 
 
 def get_filenames(files: Union[str, os.PathLike]) -> List[Path]:
@@ -28,3 +35,18 @@ def get_stats() -> Dict[str, int]:
         "mean": round(sum(tot_pages) / len(tot_pages)),
         "median": round(median(tot_pages)),
     }
+
+
+def get_double_pages(courier_id: str) -> list:
+
+    with open(CourierConfig.exclusions_file, newline='') as fp:
+        reader = csv.reader(fp, delimiter=';')
+        exclusions = [line[0] for line in reader]
+    if courier_id in exclusions:
+        return []
+
+    with open(CourierConfig.project_root / "data/courier/double_pages/double_pages.csv", "r") as fp:
+        reader = csv.reader(fp, delimiter=';')
+        pages = [x[1].split() for x in reader if courier_id in x]
+
+    return flatten(pages)
