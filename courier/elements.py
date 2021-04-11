6dadb1c8f8c976c59d8ec56b22592b5b1905b9d0
aibakeneko <aibakeneko@gmail.com>
2021-04-12 00:56:50 +0200

    fixed ambvivalent names, added comments

--- courier/elements.py
+++ courier/elements.py
@@ -27,6 +27,14 @@ def get_issue_index(courier_id: str) -> pd.DataFrame:
     return CONFIG.article_index.loc[CONFIG.article_index['courier_id'] == courier_id]
 
 
+def get_issue_content(courier_id: str) -> untangle.Element:
+    # if len(courier_id) != 6:
+    #     raise ValueError(f'Not a valid courier id "{courier_id}')
+    # if courier_id not in CONFIG.article_index.courier_id:
+    #     raise ValueError(f'{courier_id} not in article index')
+    return read_xml(list(CONFIG.pdfbox_xml_dir.glob(f'{courier_id}*.xml'))[0])
+
+
 @dataclass(order=True, frozen=True)
 class Page:
     page_number: int
@@ -37,14 +45,14 @@ class Page:
 
 
 class Article:
-    def __init__(self, record: dict, issue: 'CourierIssue'):
+    def __init__(self, record: dict, courier_issue: 'CourierIssue'):
         self.article_metadata = record
-        self.issue = issue
+        self.courier_issue = courier_issue
 
     @property
     def pages(self) -> Iterator[Page]:
         for page_number in self.article_metadata['pages']:
-            yield self.issue.get_page(page_number)
+            yield self.courier_issue.get_page(page_number)
 
     @property
     def courier_id(self) -> str:
@@ -69,33 +77,39 @@ class Article:
 
 class CourierIssue:
     def __init__(self, courier_id: str):
-        self.issue_index = get_issue_index(courier_id)
-        self.issue = read_xml(list(CONFIG.pdfbox_xml_dir.glob(f'{courier_id}*.xml'))[0])
+        self.index = get_issue_index(courier_id)
+        self.content = get_issue_content(courier_id)
         self.double_pages = get_double_pages(courier_id)
+
+        # FIXME: Add error checking, but tests must be updated
+        # if len(courier_id) != 6:
+        #     raise ValueError(f'Not a valid courier id "{courier_id}')
+        # if courier_id not in CONFIG.article_index.courier_id:
+        #     raise ValueError(f'{courier_id} not in article index')
 
     @property
     def articles(self) -> Iterator[Article]:
         with warnings.catch_warnings():
             warnings.simplefilter(action='ignore', category=FutureWarning)
-            for record in self.issue_index.to_dict('record'):
+            for record in self.index.to_dict('record'):
                 yield Article(record, self)
 
     @property
     def num_articles(self) -> int:
-        return len(self.issue_index)
+        return len(self.index)
 
     @property
     def num_pages(self) -> int:
-        return len(self.issue.document.page)
+        return len(self.content.document.page)
 
     def get_page(self, page_number: int) -> Page:
         page_delta = len([x for x in self.double_pages if x < page_number])
-        pages = [p for p in self.issue.document.page if p['number'] == str(page_number - page_delta)]
+        pages = [p for p in self.content.document.page if p['number'] == str(page_number - page_delta)]
         return Page(page_number, pages[0].cdata if len(pages) > 0 else '')
 
     def find_pattern(self, pattern: str) -> List[Tuple[int, int]]:
         page_numbers = []
-        for i, page in enumerate(self.issue.document.page, 1):
+        for i, page in enumerate(self.content.document.page, 1):
             m = re.search(pattern, page.cdata, re.IGNORECASE)
             if m:
                 page_numbers.append((i, page['number']))
