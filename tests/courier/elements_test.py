d4f4527fcc9b90aa756f9320090e1e3f9900c703
aibakeneko <aibakeneko@gmail.com>
2021-04-12 23:23:23 +0200

    updated tests

--- tests/courier/elements_test.py
+++ tests/courier/elements_test.py
@@ -3,43 +3,46 @@ from pathlib import Path
 import pytest
 import untangle
 
-import courier.elements as elements
+# import courier.elements as elements
+from courier.elements import Article, CourierIssue, Page, get_issue_content, get_issue_index, read_xml
+
+# TODO: Mock
 
 
 def test_read_xml_removes_control_chars():
 
     expected = '\n\\x01 \\x02 \\x03 \\x04 \\x05 \\x06 \\x07 \\x08\n\\x0b \\x0c \\x0e \\x0f \\x10 \\x11 \\x12 \\x13 \\x14 \\x15 \\x16 \\x17 \\x18 \\x19 \\x1a \\x1b \\x1c \\x1d \\x1e \\x1f\n'
 
-    element = elements.read_xml(Path('tests/fixtures/invalid_chars.xml'))
+    content = read_xml(Path('tests/fixtures/invalid_chars.xml'))
 
-    assert isinstance(element, untangle.Element)
-    assert element.content.cdata == expected
+    assert isinstance(content, untangle.Element)
+    assert content.content.cdata == expected
 
 
 def test_get_courier_issue_index_return_expected_values():
-    assert len(elements.get_issue_index('061468')) == 3
+    assert len(get_issue_index('061468')) == 3
 
 
 def test_get_courier_issue_index_with_invalid_id_raises_value_error():
     with pytest.raises(ValueError, match='Not a valid courier id'):
-        elements.get_issue_index('')
+        get_issue_index('')
     with pytest.raises(ValueError, match='Not a valid courier id'):
-        elements.get_issue_index('0')
+        get_issue_index('0')
     with pytest.raises(ValueError, match='not in article index'):
-        elements.get_issue_index('000000')
+        get_issue_index('000000')
 
 
 def test_get_issue_content_return_expected_values():
-    courier_issue = elements.CourierIssue('061468')
+    courier_issue = CourierIssue('061468')
     assert isinstance(courier_issue.content, untangle.Element)
     assert 'MARCH 1964' in courier_issue.content.document.page[2].cdata
 
 
 def test_get_issue_content_with_invalid_id_raises_value_error():
     with pytest.raises(ValueError, match='Not a valid courier id'):
-        elements.get_issue_content('0')
+        get_issue_content('0')
     with pytest.raises(ValueError, match='not in article index'):
-        elements.get_issue_content('000000')
+        get_issue_content('000000')
 
 
 @pytest.mark.parametrize(
@@ -52,8 +55,8 @@ def test_get_issue_content_with_invalid_id_raises_value_error():
 )
 def test_create_page(input_pn, input_text, expected_pn, expected_text):
 
-    result = elements.Page(input_pn, input_text)
-    assert isinstance(result, elements.Page)
+    result = Page(input_pn, input_text)
+    assert isinstance(result, Page)
 
     assert isinstance(result.page_number, int)
     assert result.page_number == expected_pn
@@ -62,9 +65,20 @@ def test_create_page(input_pn, input_text, expected_pn, expected_text):
     assert result.text == expected_text
 
 
+def test_create_article():
+    courier_issue = CourierIssue('061468')
+    index = courier_issue.index.to_dict('record')[0]
+    article = Article(index, courier_issue)
+    assert article.courier_id == '061468'
+    assert article.record_number == '61469'
+    assert 'Eleven centuries' in article.title
+    assert article.year == '1964'
+    assert article.publication_date == '1964'
+
+
 def test_create_courier_issue():
-    courier_issue = elements.CourierIssue('061468')
-    assert isinstance(courier_issue, elements.CourierIssue)
+    courier_issue = CourierIssue('061468')
+    assert isinstance(courier_issue, CourierIssue)
 
     assert courier_issue.num_articles == 3
     assert courier_issue.num_pages == 34
@@ -73,9 +87,9 @@ def test_create_courier_issue():
 
 def test_create_non_existing_issue_raises_value_error():
     with pytest.raises(ValueError, match='Not a valid courier id'):
-        elements.CourierIssue('0')
+        CourierIssue('0')
     with pytest.raises(ValueError, match='not in article index'):
-        elements.CourierIssue('000000')
+        CourierIssue('000000')
 
 
 @pytest.mark.parametrize(
@@ -88,11 +102,24 @@ def test_create_non_existing_issue_raises_value_error():
     ],
 )
 def test_courier_issues_has_correct_double_pages(courier_id, expected):
-    result = elements.CourierIssue(courier_id).double_pages
+    result = CourierIssue(courier_id).double_pages
     assert result == expected
 
 
 def test_courier_issue_has_correct_index():
-    issue_1 = elements.CourierIssue('061468')
-    assert not issue_1.index.empty
-    assert issue_1.index.size == 24
+    courier_issue = CourierIssue('061468')
+    assert not courier_issue.index.empty
+    assert courier_issue.index.size == 24
+
+
+@pytest.mark.parametrize(
+    'courier_id, pattern, expected',
+    [
+        ('061468', 'MARCH 1964', [(1, '1'), (3, '3')]),
+        ('061468', r'a.*open.*world', [(1, '1')]),
+        ('061468', 'nonmatchingpattern', []),
+    ],
+)
+def test_courier_issue_find_pattern_returns_expected_values(courier_id, pattern, expected):
+    result = CourierIssue(courier_id).find_pattern(pattern)
+    assert result == expected
