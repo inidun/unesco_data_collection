a2e00765d9f03c095a047fe44f783abcdc3c5ffb
aibakeneko <aibakeneko@gmail.com>
2021-04-16 16:41:38 +0200

    added test

--- tests/courier/utils_test.py
+++ tests/courier/utils_test.py
@@ -1,5 +1,5 @@
 from courier.config import get_config
-from courier.utils import get_stats
+from courier.utils import get_filenames, get_stats
 
 CONFIG = get_config()
 
@@ -11,3 +11,20 @@ def test_get_stats(monkeypatch):
     assert stats['mean'] == 8
     assert stats['median'] == 8
     assert stats['pages'] == 8
+
+
+def test_get_filenames_only_returns_pdf_files(tmp_path):
+    tempdir = tmp_path / 'files'
+    tempdir.mkdir()
+    pdf_file = tempdir / 'test.pdf'
+    txt_file = tempdir / 'test.txt'
+    pdf_file.touch()
+    txt_file.touch()
+    assert len(list(tempdir.iterdir())) == 2
+    assert pdf_file in list(tempdir.iterdir())
+    assert txt_file in list(tempdir.iterdir())
+    assert len(get_filenames(tempdir)) == 1
+    assert pdf_file in get_filenames(tempdir)
+    assert txt_file not in get_filenames(tempdir)
+    assert get_filenames(txt_file) == []
+    assert get_filenames(pdf_file) == [pdf_file]
