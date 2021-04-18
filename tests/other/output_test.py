c691ab2c6190015f32dd9b66114ebfdb4f471cd2
aibakeneko <aibakeneko@gmail.com>
2021-04-18 16:01:58 +0200

    moved test

--- tests/other/output_test.py
+++ tests/other/output_test.py
@@ -9,11 +9,11 @@ CONFIG = get_config()
 def test_all_files_extracted():
     stats = get_stats()
     assert len(list(CONFIG.pdf_dir.iterdir())) == stats['files'] == 671
+    assert len(list(CONFIG.pdfbox_xml_dir.iterdir())) == stats['files'] == 671
     assert len(list(CONFIG.pdfbox_txt_dir.iterdir())) == stats['pages'] == 27336
 
 
 def test_all_articles_extracted():
     assert len(list((CONFIG.article_output_dir / 'xml').iterdir())) == len(CONFIG.article_index)
     assert len(list((CONFIG.article_output_dir / 'txt').iterdir())) == len(CONFIG.article_index)
-
     assert len(pd.read_csv(CONFIG.article_output_dir / 'article_index.csv', sep='\t')) == 7612
