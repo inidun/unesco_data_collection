0c76f955287ae36cd02e918f5375ec97bfec7df4
aibakeneko <aibakeneko@gmail.com>
2021-04-28 17:19:53 +0200

    updated tests

--- tests/courier/split_article_pages_test.py
+++ tests/courier/split_article_pages_test.py
@@ -1,8 +1,15 @@
 import pandas as pd
+import pytest
 
 from courier.config import get_config
 from courier.overlap_check import get_overlapping_pages
-from courier.split_article_pages import create_regexp, get_stats, save_stats
+from courier.split_article_pages import (
+    create_regexp,
+    find_title_by_fuzzymatch,
+    find_title_by_regex,
+    get_stats,
+    save_stats,
+)
 
 CONFIG = get_config()
 
@@ -14,21 +21,31 @@ def test_create_regexp():
     assert expr == '[^a-zåäö]+nice[^a-zåäö]+and[^a-zåäö]+happy[^a-zåäö]+title[^a-zåäö]+maybe'
 
 
-def test_get_stats_collects_both_found_and_not_found():
+def test_get_stats_returns_dataframe_or_correct_size():
     stats = get_stats(
         article_index=CONFIG.article_index,
         overlap=get_overlapping_pages(CONFIG.article_index)[:3],
     )
-    assert stats.shape == (3, 6)
+    assert stats.shape == (3, 7)
 
 
-def test_get_stats_returns_expected_values():
+@pytest.mark.parametrize(
+    'courier_id, method, expected',
+    [
+        (14255, find_title_by_regex, ['014255', 36, 36, 2, 0, 2, 1]),
+        (14255, find_title_by_fuzzymatch, ['014255', 36, 36, 2, 0, 2, 1]),
+        (59301, find_title_by_regex, ['059301', 30, 30, 2, 1, 1, 1]),
+        (59301, find_title_by_fuzzymatch, ['059301', 30, 30, 2, 0, 2, 1]),
+    ],
+)
+def test_get_stats_returns_expected_values(courier_id, method, expected):
     stats = get_stats(
-        article_index=CONFIG.article_index.loc[14255],
-        overlap=get_overlapping_pages(CONFIG.article_index.loc[14255]),
+        article_index=CONFIG.article_index.loc[courier_id],
+        overlap=get_overlapping_pages(CONFIG.article_index.loc[courier_id]),
+        match_function=method,
     )
-    assert stats.shape == (1, 6)
-    assert stats.values.tolist()[0] == ['014255', 36, 36, 2, 0, 2]
+    result = stats.values.tolist()[0]
+    assert result == expected
 
 
 def test_get_stats_logs_mismatches(caplog):
@@ -39,6 +56,7 @@ def test_get_stats_logs_mismatches(caplog):
     assert 'Page count mismatch' in caplog.text
 
 
+@pytest.mark.skip(reason='Update')
 def test_save_stats(monkeypatch, tmp_path):
     monkeypatch.setattr(CONFIG, 'article_index', CONFIG.article_index.loc[14255])
     save_stats(tmp_path / 'overlap_stats.csv')
