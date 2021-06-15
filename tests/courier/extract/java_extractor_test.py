d1396875c92febb5865005ca52d67607f7d11c46
aibakeneko <aibakeneko@gmail.com>
2021-06-15 12:12:54 +0200

    Added test for java_extractor

---
+++ tests/courier/extract/java_extractor_test.py
@@ -0,0 +1,19 @@
+from courier.config import get_config
+from courier.extract.java_extractor import ExtractedIssue, JavaExtractor
+
+CONFIG = get_config()
+
+
+# TODO: Verify that titles are on correct pages!
+
+
+def test_java_extractor():
+    extractor: JavaExtractor = JavaExtractor()
+    filename = CONFIG.test_files_dir / 'test.pdf'
+    filename = CONFIG.pdf_dir / '012656engo.pdf'
+    # filename: str = str(CONFIG.test_files_dir / 'test.pdf')
+
+    # expected_output = CONFIG.test_files_dir / 'test.txt'
+
+    issue: ExtractedIssue = extractor.extract_issue(filename)
+    assert issue is not None
