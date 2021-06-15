5612becbf38ee6cddbd431dffce9d0acba9af345
aibakeneko <aibakeneko@gmail.com>
2021-06-15 15:48:14 +0200

    Updated tests

--- tests/courier/elements_test.py
+++ tests/courier/elements_test.py
@@ -3,12 +3,21 @@ from pathlib import Path
 import pytest
 import untangle
 
-from courier.elements import Article, CourierIssue, Page, get_pdf_issue_content, get_xml_issue_content, read_xml
+from courier.elements import (
+    Article,
+    CourierIssue,
+    ExtractArticles,
+    Page,
+    get_pdf_issue_content,
+    get_xml_issue_content,
+    read_xml,
+)
 from courier.extract.java_extractor import ExtractedIssue
 
 # TODO: Mock
 
 
+@pytest.mark.legacy
 def test_read_xml_removes_control_chars():
     expected = '\n\\x01 \\x02 \\x03 \\x04 \\x05 \\x06 \\x07 \\x08\n\\x0b \\x0c \\x0e \\x0f \\x10 \\x11 \\x12 \\x13 \\x14 \\x15 \\x16 \\x17 \\x18 \\x19 \\x1a \\x1b \\x1c \\x1d \\x1e \\x1f\n'
     content = read_xml(Path('tests/fixtures/invalid_chars.xml'))
@@ -21,6 +30,7 @@ def test_get_xml_issue_content_return_expected_values():
     content: ExtractedIssue = get_xml_issue_content(courier_id='012656')
     assert isinstance(content, ExtractedIssue)
     assert 'SEPTEMBER 1966' in str(content.pages[2])
+
 
 def test_get_xml_issue_content_with_invalid_id_raises_value_error():
     with pytest.raises(ValueError, match='Not a valid courier id'):
@@ -149,3 +159,8 @@ def test_to_pdf_page_number():
     assert 15 == issue.to_pdf_page_number(15)
     assert 18 == issue.to_pdf_page_number(18)
     assert 19 == issue.to_pdf_page_number(20)
+
+
+def test_main():
+    issue = CourierIssue('012656')
+    ExtractArticles.extract(issue)
