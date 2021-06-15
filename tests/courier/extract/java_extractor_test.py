5d383aebdee1cfe6796658b6ccabdb4dbb65f23d
aibakeneko <aibakeneko@gmail.com>
2021-06-15 12:33:43 +0200

    Tidied up

--- tests/courier/extract/java_extractor_test.py
+++ tests/courier/extract/java_extractor_test.py
@@ -9,10 +9,8 @@ CONFIG = get_config()
 
 def test_java_extractor():
     extractor: JavaExtractor = JavaExtractor()
-    filename = CONFIG.test_files_dir / 'test.pdf'
     filename = CONFIG.pdf_dir / '012656engo.pdf'
     # filename: str = str(CONFIG.test_files_dir / 'test.pdf')
-
     # expected_output = CONFIG.test_files_dir / 'test.txt'
 
     issue: ExtractedIssue = extractor.extract_issue(filename)
