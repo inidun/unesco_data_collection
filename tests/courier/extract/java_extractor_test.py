59a6d9f5e43560d3fc7eca0b850e196e5e9c250b
aibakeneko <aibakeneko@gmail.com>
2021-06-30 14:17:30 +0200

    Resolves #34

--- tests/courier/extract/java_extractor_test.py
+++ tests/courier/extract/java_extractor_test.py
@@ -5,15 +5,36 @@ from courier.extract.java_extractor import ExtractedIssue, JavaExtractor
 
 CONFIG = get_config()
 
-# TODO: Verify that titles are on correct pages!
-
 
 @pytest.mark.java
 def test_java_extractor():
     extractor: JavaExtractor = JavaExtractor()
     filename = CONFIG.pdf_dir / '012656engo.pdf'
-    # filename: str = str(CONFIG.test_files_dir / 'test.pdf')
-    # expected_output = CONFIG.test_files_dir / 'test.txt'
-
     issue: ExtractedIssue = extractor.extract_issue(filename)
-    assert issue is not None
+
+    assert issue.page_count == 35
+    assert issue.pages[1].pdf_page_number == 2
+    assert 'TREASURES' in issue.pages[1].content
+    assert len(issue.pages[1].titles) == 1
+    assert len([title for title, _ in issue.pages[1].titles if 'TREASURE' in title]) != 0
+
+
+# TODO: Parametrize
+@pytest.mark.java
+def test_titles_on_correct_pages():
+    extractor: JavaExtractor = JavaExtractor()
+    filename = CONFIG.pdf_dir / '012656engo.pdf'
+    issue: ExtractedIssue = extractor.extract_issue(filename)
+
+    assert titles_in_content(issue)
+
+
+def titles_in_content(issue: ExtractedIssue) -> bool:
+    """Returns true iff for each page in `issue` all of the page's titles are contained within the page's content"""
+    # pylint: disable=use-a-generator
+    return all(
+        [
+            all([title in issue.pages[i].content for title in [title for title, _ in issue.pages[i].titles]])
+            for i in range(0, issue.page_count)
+        ]
+    )
