d137462b2dba6cafa81c2895040f8b010c29f993
aibakeneko <aibakeneko@gmail.com>
2021-07-09 16:36:32 +0200

    Added new custom pdfbox implementation Reverted to PDFBox 2.0.24 for compatibility

--- courier/extract/java_extractor.py
+++ courier/extract/java_extractor.py
@@ -3,21 +3,30 @@
 
 import os
 from dataclasses import dataclass
+from pathlib import Path
 from typing import List, Tuple, Union
 
 import jpype
 import jpype.imports
+from appdirs import AppDirs
 
 from courier.config import get_config
 
 CONFIG = get_config()
-pdfcourier2text_path = CONFIG.project_root / 'courier/lib/pdfbox-app-3.0.0-SNAPSHOT.jar'
+
+# pdfcourier2text_path = CONFIG.project_root / 'courier/lib/pdfbox-app-3.0.0-SNAPSHOT.jar'
+
+cache_dir = Path(AppDirs('python-pdfbox').user_cache_dir)
+pdfbox_path = list(cache_dir.glob('pdfbox-app-*.jar'))[-1]
+pdfcourier2text_path = CONFIG.project_root / 'courier/lib/pdfextract-1.0-SNAPSHOT.jar'
 
 if not jpype.isJVMStarted():
+    jpype.addClassPath(pdfbox_path)
     jpype.addClassPath(pdfcourier2text_path)
     # jpype.startJVM(convertStrings=False)
     jpype.startJVM('-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.NoOpLog', convertStrings=False)
-import org.apache.pdfbox.tools as pdfbox_tools  # isort: skip  # noqa: E402
+# import org.apache.pdfbox.tools as pdfbox_tools  # isort: skip  # noqa: E402
+import se.umu.humlab.pdfextract as pdfbox_tools  # isort: skip  # noqa: E402
 
 
 @dataclass
@@ -39,6 +48,9 @@ class ExtractedIssue:
 
     def __len__(self) -> int:
         return len(self.pages or [])
+
+    def __str__(self) -> str:
+        return '\n\n'.join([str(p.content) for p in self.pages])
 
 
 # TODO: Use this in `pdfbox_extractor` or new `custom_pdfbox_extractor`
