0a0eabb15e40d968f0f25ab942ed63228289fc62
aibakeneko <aibakeneko@gmail.com>
2021-09-21 18:09:03 +0200

    Fixed f-strings

--- tests/legal_instruments/legal_instruments_test.py
+++ tests/legal_instruments/legal_instruments_test.py
@@ -19,13 +19,13 @@ def project_root():
 
 
 def get_sample_convention_html():
-    with open("{}/tests/fixtures/sample_response_page.html".format(project_root()), "r") as fp:
+    with open(f"{project_root()}/tests/fixtures/sample_response_page.html", "r") as fp:
         return fp.read()
 
 
 def get_sample_convention_index_html():
     # http://portal.unesco.org/en/ev.php-URL_ID=12025&URL_DO=DO_TOPIC&URL_SECTION=-471.html
-    with open("{}/tests/fixtures/sample_response_index_page.html".format(project_root()), "r") as fp:
+    with open(f"{project_root()}/tests/fixtures/sample_response_index_page.html", "r") as fp:
         return fp.read()
 
 
