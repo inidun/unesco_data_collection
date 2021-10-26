89e1b2a70485578d73c69031f77f56d683b7b4f3
aibakeneko <aibakeneko@gmail.com>
2021-10-26 11:45:36 +0200

    Added FIXME

--- courier/elements/__init__.py
+++ courier/elements/__init__.py
@@ -1,6 +1,6 @@
 from .assign_page_service import AssignPageService
 from .consolidate_text_service import ConsolidateTextService, fuzzy_find_title
-from .elements import *
+from .elements import *  # FIXME: export only necessary
 from .export_articles import export_articles
 from .statistics import IssueStatistics
 from .utils import get_pdf_issue_content, get_xml_issue_content, read_xml
