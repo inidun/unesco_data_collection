82ae71aa3e4053e1342dd1defb3c78849a83a1a2
aibakeneko <aibakeneko@gmail.com>
2021-10-26 16:49:26 +0200

    Updated IssueStatistics: fields instead of props Added num_errors

--- courier/elements/statistics.py
+++ courier/elements/statistics.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, Dict, List, Tuple
 
 from courier.config import get_config
@@ -14,48 +14,35 @@ class IssueStatistics:
 
     issue: CourierIssue
 
-    @property
-    def total_pages(self) -> int:
-        """Number of pages in issue"""
-        return len(self.issue)
+    total_pages: int = field(init=False)
+    assigned_pages: int = field(init=False)
+    consolidated_pages: int = field(init=False)
+    expected_article_pages: int = field(init=False)
+    number_of_articles: int = field(init=False)
+    year: int = field(init=False)
+    missing_pages: List[Tuple[str, int, int]] = field(init=False)
+    num_missing_pages: int = field(init=False)
+    errors: List[Dict[str, Any]] = field(init=False)
+    num_errors: int = field(init=False)
 
-    @property
-    def assigned_pages(self) -> int:
-        """Number of pages in issue assigned to an article"""
-        return len(self.issue.get_assigned_pages())
+    def __post_init__(self) -> None:
 
-    @property
-    def consolidated_pages(self) -> int:
-        """Number of consolidated pages in issue"""
-        return len(self.issue.get_consolidated_pages())
-
-    @property
-    def expected_article_pages(self) -> int:
-        """Number of article pages in issue according to index"""
-        return len(self.issue.get_article_pages())
-
-    @property
-    def number_of_articles(self) -> int:
-        """Number of articles in issue"""
-        return self.issue.num_articles
-
-    @property
-    def year(self) -> int:
-        return CONFIG.issue_index.loc[int(self.issue.courier_id.lstrip('0'))].year
-
-    @property
-    def missing_pages(self) -> List[Tuple[str, int, int]]:
-        return [(x.courier_id or '', x.record_number or 0, len(x.pages) - len(x.texts)) for x in self.issue.articles]
-
-    @property
-    def num_missing_pages(self) -> int:
-        return sum([len(x.pages) - len(x.texts) for x in self.issue.articles])
-
-    @property
-    def errors(self) -> List[Dict[str, Any]]:
-        return flatten(
-            [[error.to_dict() for error in page.errors] for page in self.issue.pages if len(page.errors) != 0]
+        self.total_pages: int = len(self.issue)
+        self.assigned_pages: int = len(self.issue.get_assigned_pages())
+        self.consolidated_pages: int = len(self.issue.get_consolidated_pages())
+        self.expected_article_pages: int = len(self.issue.get_article_pages())
+        self.number_of_articles: int = self.issue.num_articles
+        self.year: int = CONFIG.issue_index.loc[int(self.issue.courier_id.lstrip('0'))].year
+        self.missing_pages: List[Tuple[str, int, int]] = [
+            (x.courier_id or '', x.record_number or 0, len(x.pages) - len(x.texts)) for x in self.issue.articles
+        ]
+        self.num_missing_pages: int = sum(
+            [len(x.pages) - len(x.texts) for x in self.issue.articles]
+        )  # FIXME: Should return (expected - consolidated) instead? Rename?
+        self.errors: List[Dict[str, Any]] = flatten(
+            [[error.asdict for error in page.errors] for page in self.issue.pages if len(page.errors) != 0]
         )
+        self.num_errors: int = len(self.errors)
 
 
 if __name__ == '__main__':
