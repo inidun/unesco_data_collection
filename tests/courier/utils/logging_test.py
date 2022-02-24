67f7840f9ce3838c726c2e70fe733dc14cc06681
aibakeneko <aibakeneko@gmail.com>
2022-02-24 20:02:45 +0100

    Updated test

--- tests/courier/utils/logging_test.py
+++ tests/courier/utils/logging_test.py
@@ -18,19 +18,30 @@ def test_file_logger():
 
 def test_file_logger_wrapping():
     with TemporaryDirectory() as output_dir:
+
         logfile1 = Path(output_dir) / 'test1.log'
         logfile2 = Path(output_dir) / 'test2.log'
+        logfile3 = Path(output_dir) / 'test3.log'
 
         with file_logger(logfile1, format='{message}') as logger1:
             logger1.info('1')
             with file_logger(logfile2, format='{message}') as logger2:
+                logger1.info('12')
                 logger2.info('2')
+                with file_logger(logfile3, format='{message}') as logger3:
+                    logger1.info('123')
+                    logger2.info('23')
+                    logger3.info('3')
 
         assert (Path(output_dir) / 'test1.log').exists()
         assert (Path(output_dir) / 'test2.log').exists()
+        assert (Path(output_dir) / 'test3.log').exists()
 
         with open(logfile1, 'r', encoding='utf-8') as fp:
-            assert fp.read() == '1\n'
+            assert fp.read() == '1\n12\n123\n'
 
         with open(logfile2, 'r', encoding='utf-8') as fp:
-            assert fp.read() == '2\n'
+            assert fp.read() == '2\n23\n'
+
+        with open(logfile3, 'r', encoding='utf-8') as fp:
+            assert fp.read() == '3\n'
