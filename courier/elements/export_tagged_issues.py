3c9b818fb734b9279214d4c76a77e71a15d636ba
aibakeneko <aibakeneko@gmail.com>
2022-03-02 16:17:11 +0100

    Case: one article on page. Using get_best_candidate instead of ConsolidateTextService Escaped special chars Added XML-tags Ref #60

--- courier/elements/export_tagged_issues.py
+++ courier/elements/export_tagged_issues.py
@@ -1,6 +1,7 @@
 # pylint: disable=redefined-outer-name
 import os
 from datetime import datetime
+from html import escape
 from pathlib import Path
 from typing import List, Union
 
@@ -8,7 +9,7 @@ from loguru import logger
 
 from courier.config import get_config
 from courier.elements.assign_page_service import AssignPageService
-from courier.elements.consolidate_text_service import ConsolidateTextService
+from courier.elements.consolidate_text_service import get_best_candidate
 from courier.elements.elements import CourierIssue
 
 CONFIG = get_config()
@@ -18,30 +19,47 @@ def export_tagged_issue(
     courier_id: str,
     export_folder: Union[str, os.PathLike] = CONFIG.articles_dir / 'exported',
 ) -> None:
+
     issue: CourierIssue = CourierIssue(courier_id)
-
     issue_article_index = CONFIG.get_issue_article_index(courier_id)
-
-    AssignPageService().assign(issue)
-    ConsolidateTextService().consolidate(issue)
-    Path(export_folder).mkdir(parents=True, exist_ok=True)
     year = issue_article_index[0]['year']
+    AssignPageService().assign(issue)
 
     texts: List[str] = []
+    texts.append('<?xml version="1.0" encoding="UTF-8"?>')
+    texts.append(f'<document id="{courier_id}">')
 
     for page in issue.pages:
         texts.append(f'<page i="{page.page_number}">')
+
         if len(page.articles) == 0:
-            texts.append(page.text)
-        # TODO: Use position
+            texts.append(escape(page.text, quote=False))
+
         elif len(page.articles) == 1:
-            texts.append(f'<article record_number="{page.articles[0].catalogue_title} " i="{page.page_number}">')
-            texts.append(page.text)
+
+            article = page.articles[0]
+            pos, _ = get_best_candidate(article.catalogue_title, page.titles)
+
+            if pos is None:
+                texts.append(
+                    f'<article record_number="{article.record_number}" title="{article.catalogue_title}" i="{page.page_number}">'
+                )
+                texts.append(escape(page.text, quote=False))
+
+            else:
+                logger.info(f'{courier_id} {page.page_number} {pos} {article.catalogue_title}')
+                texts.append(escape(page.text[:pos], quote=False))
+                texts.append(
+                    f'<article record_number="{article.record_number}" title="{article.catalogue_title}" i="{page.page_number}">'
+                )
+                texts.append(escape(page.text[pos:], quote=False))
+
             texts.append('</article>')
-
         texts.append('</page>')
+    texts.append('</document>')
 
-    filename: Path = Path(export_folder) / f'tagged_{year}_{courier_id}.txt'
+    Path(export_folder).mkdir(parents=True, exist_ok=True)
+    filename: Path = Path(export_folder) / f'tagged_{year}_{courier_id}.xml'
 
     with open(filename, 'w', encoding='utf-8') as fp:
         fp.write('\n'.join(texts))
@@ -49,16 +67,16 @@ def export_tagged_issue(
 
 def main() -> None:
     export_folder: Path = CONFIG.articles_dir / f'tagged_issues_{datetime.now().strftime("%Y-%m-%dT%H%M")}'
-
-    logger.trace('courier_id;year;record_number;assigned;not_found;total')
+    logger.info('courier_id;year;record_number;assigned;not_found;total')
 
     courier_ids = [x[:6] for x in CONFIG.get_courier_ids()]
+    # courier_ids = ['049631', '074686', '066148', '067651', '070186', '070464', '068057']
 
     for courier_id in courier_ids:
-        if courier_id not in CONFIG.article_index.courier_id.values:
-            continue
-
-        export_tagged_issue(courier_id, export_folder)
+        try:
+            export_tagged_issue(courier_id, export_folder)
+        except ValueError as e:
+            logger.info(e)
 
 
 if __name__ == '__main__':
