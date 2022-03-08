b3b4c23431157aef7d92f064887b4be6f60f1ac4
aibakeneko <aibakeneko@gmail.com>
2022-03-08 13:08:24 +0100

    Added tests. Ref #61.

---
+++ tests/courier/elements/export_tagged_issues_test.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from courier.config import get_config
+from courier.elements import CourierIssue
+from courier.elements.export_tagged_issues import export_tagged_issue
+
+CONFIG = get_config()
+
+
+def test_pdf_has_correct_number_of_titles(monkeypatch):
+    monkeypatch.setattr(CONFIG, 'pdf_dir', Path('tests/fixtures/courier/custom_pdf'))
+    monkeypatch.setattr(CONFIG, 'metadata_file', Path('tests/fixtures/courier/custom_pdf/metadata.csv'))
+
+    courier_id = '012356'
+    issue: CourierIssue = CourierIssue(courier_id)
+
+    assert [len(x.titles) for x in issue.pages] == [3, 3, 1]
+
+
+def test_export_tagged(monkeypatch, tmp_path):
+    monkeypatch.setattr(CONFIG, 'pdf_dir', Path('tests/fixtures/courier/custom_pdf'))
+    monkeypatch.setattr(CONFIG, 'metadata_file', Path('tests/fixtures/courier/custom_pdf/metadata.csv'))
+
+    courier_id = '012356'
+
+    export_tagged_issue(courier_id=courier_id, export_folder=tmp_path)
+
+    assert (tmp_path / f'tagged_2022_{courier_id}.md').exists()
