160d215b26fdd0e90e49a423e0751ffd7948cd5c
aibakeneko <aibakeneko@gmail.com>
2022-03-16 15:23:55 +0100

    Added test

--- tests/courier/overlap_check_test.py
+++ tests/courier/overlap_check_test.py
@@ -1,10 +1,16 @@
+from io import StringIO
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
 import pandas as pd
+import pytest
 
 from courier.config import get_config
-from courier.overlap_check import get_overlapping_pages, save_overlapping_pages
+from courier.overlap_check import (
+    get_articles_with_overlap_of_two_or_more_other_articles,
+    get_overlapping_pages,
+    save_overlapping_pages,
+)
 
 CONFIG = get_config()
 
@@ -22,3 +28,23 @@ def test_save_overlapping_pages():
         overlapping_pages = get_overlapping_pages(CONFIG.article_index)
         save_overlapping_pages(overlapping_pages, (Path(output_dir) / 'overlap.csv'))
         assert (Path(output_dir) / 'overlap.csv').exists()
+
+
+@pytest.fixture
+def article_index():
+    statistics = StringIO(
+        """courier_id;year;record_number;pages;catalogue_title
+111111;2020;111;[1];a1
+111111;2020;111;[1];a2
+111111;2020;111;[1];a3
+222222;2020;222;[1];b1
+222222;2020;222;[1];b2
+"""
+    )
+    return pd.read_csv(statistics, sep=';')
+
+
+def test_get_articles_with_overlap_of_two_or_more_other_articles(article_index):  # pylint: disable=redefined-outer-name
+    df = get_articles_with_overlap_of_two_or_more_other_articles(article_index)
+    assert df.shape == (1, 3)
+    assert df.iloc[0].record_number == 111
