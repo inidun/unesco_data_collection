b6a0847f8a652f8a604ebe2462a04c9d86f33fd5
aibakeneko <aibakeneko@gmail.com>
2022-11-09 13:37:28 +0100

    Resolves #77

--- courier/cli/tagged2article.py
+++ courier/cli/tagged2article.py
@@ -3,15 +3,16 @@ from os.path import basename, splitext
 import click
 from loguru import logger
 
+from courier.extract.utils import get_filenames
 from courier.scripts.tagged_issue_to_articles import get_issue_articles, store_article_text
 
 
 @click.command()
-@click.argument('filenames', nargs=-1)
+@click.argument('source', nargs=1)
 @click.argument('target_folder', nargs=1)
-def main(filenames: list[str], target_folder: str) -> None:
+def main(source: str, target_folder: str) -> None:
 
-    for filename in filenames:
+    for filename in get_filenames(source, 'md'):
         _, year, courier_id = splitext(basename(filename))[0].split('_')
         articles = get_issue_articles(filename)
         store_article_text(articles, target_folder, year, courier_id)
